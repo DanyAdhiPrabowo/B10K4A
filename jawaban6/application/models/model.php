710ec4933d2640f4fb77527daadfbfb8adbd7a3f
DanyAdhi <45444857+DanyAdhi@users.noreply.github.com>
2019-05-18 14:49:12 +0700

    no6

---
+++ jawaban6/application/models/model.php
@@ -0,0 +1,23 @@
+<?php 
+
+class Model extends CI_Model{
+
+
+	public function get_all($table)
+	{
+		return $this->db->get($table);
+	}
+
+	public function get_by($table,$id, $where)
+	{
+		return $this->db->get_where($table, [$id=>$where]);
+	}
+
+	public function save($table,$data)
+	{
+		$this->db->insert($table, $data);
+	}
+
+
+}
+ ?>
